b3fc3f55facc55c070355da843ed2fdb88e35f30
puneet <imhtcy312@gmail.com>
2025-03-10 08:55:17 +0530

    resolve cors issue

--- server/index.ts
+++ server/index.ts
@@ -10,10 +10,10 @@ app.use(express.urlencoded({ extended: true }));
 dbConnect();
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    // credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    origin: variables.CORS_ORIGIN,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use((req, res, next) => {
